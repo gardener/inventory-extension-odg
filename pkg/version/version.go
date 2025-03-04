e9239b6ff6c10ec1bfb48f58a98c9cf62e7a19a2
Marin Nikolov <marin.nikolov@sap.com>
2025-03-04 12:24:55 +0200

    version: initial commit of the `version` package

---
+++ pkg/version/version.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package version
+
+// Version is the version of the Gardener Inventory
+var Version = "unknown"
