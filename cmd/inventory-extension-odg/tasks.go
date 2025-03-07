6ebe67b0f018fa49aa23973750e9eae8a6c62169
Marin Nikolov <marin.nikolov@sap.com>
2025-03-07 11:54:14 +0200

    cmd: add `worker` and `tasks` sub-commands

---
+++ cmd/inventory-extension-odg/tasks.go
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/gardener/inventory/pkg/core/registry"
+	"github.com/hibiken/asynq"
+	"github.com/urfave/cli/v2"
+
+	_ "github.tools.sap/kubernetes/inventory-extension-odg/pkg/odg/tasks"
+)
+
+// NewTasksCommand returns a new [cli.Command] for tasks-related operations.
+func NewTasksCommand() *cli.Command {
+	cmd := &cli.Command{
+		Name:    "tasks",
+		Usage:   "task operations",
+		Aliases: []string{"t"},
+		Subcommands: []*cli.Command{
+			{
+				Name:    "list",
+				Usage:   "list registered tasks",
+				Aliases: []string{"ls"},
+				Action:  execTaskListCommand,
+			},
+		},
+	}
+
+	return cmd
+}
+
+// execTaskListCommand lists the tasks from the default registry
+func execTaskListCommand(ctx *cli.Context) error {
+	tasks := make([]string, 0)
+	_ = registry.TaskRegistry.Range(func(name string, _ asynq.Handler) error {
+		tasks = append(tasks, name)
+		return nil
+	})
+
+	slices.Sort(tasks)
+	for _, name := range tasks {
+		fmt.Println(name)
+	}
+
+	return nil
+}
---
+++ cmd/inventory-extension-odg/worker.go
@@ -0,0 +1,127 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+
+	dbclient "github.com/gardener/inventory/pkg/clients/db"
+	"github.com/gardener/inventory/pkg/core/registry"
+	asynqutils "github.com/gardener/inventory/pkg/utils/asynq"
+	workerutils "github.com/gardener/inventory/pkg/utils/asynq/worker"
+	dbutils "github.com/gardener/inventory/pkg/utils/db"
+	slogutils "github.com/gardener/inventory/pkg/utils/slog"
+	"github.com/hibiken/asynq"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/extra/bundebug"
+	"github.com/urfave/cli/v2"
+
+	"github.tools.sap/kubernetes/inventory-extension-odg/pkg/config"
+)
+
+// NewWorkerCommand returns a new [cli.Command] for worker-related operations.
+func NewWorkerCommand() *cli.Command {
+	cmd := &cli.Command{
+		Name:    "worker",
+		Usage:   "worker operations",
+		Aliases: []string{"w"},
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:     "config",
+				Usage:    "path to extension config file",
+				Required: true,
+				Aliases:  []string{"file"},
+				EnvVars:  []string{"INVENTORY_EXTENSION_CONFIG"},
+			},
+		},
+		Subcommands: []*cli.Command{
+			{
+				Name:    "start",
+				Usage:   "start worker process",
+				Aliases: []string{"s"},
+				Action:  execWorkerStartCommand,
+			},
+		},
+	}
+
+	return cmd
+}
+
+// newDB creates a new [bun.DB] database client based on the given config.
+func newDB(conf *config.Config) (*bun.DB, error) {
+	db, err := dbutils.NewFromConfig(conf.Database)
+	if err != nil {
+		return nil, err
+	}
+	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(conf.Debug)))
+
+	return db, nil
+}
+
+// newWorker creates a new [workerutils.Worker] from the given config.
+func newWorker(conf *config.Config) *workerutils.Worker {
+	redisClientOpt := asynqutils.NewRedisClientOptFromConfig(conf.Redis)
+
+	opts := make([]workerutils.Option, 0)
+	logLevel := asynq.InfoLevel
+	if conf.Debug {
+		logLevel = asynq.DebugLevel
+	}
+
+	opts = append(opts, workerutils.WithLogLevel(logLevel))
+	opts = append(opts, workerutils.WithErrorHandler(asynqutils.NewDefaultErrorHandler()))
+	worker := workerutils.NewFromConfig(redisClientOpt, conf.Worker, opts...)
+
+	// Configure middlewares
+	middlewares := []asynq.MiddlewareFunc{
+		asynqutils.NewLoggerMiddleware(slog.Default()),
+		asynqutils.NewMeasuringMiddleware(),
+	}
+	worker.UseMiddlewares(middlewares...)
+
+	return worker
+}
+
+// execStartCommand starts the worker
+func execWorkerStartCommand(ctx *cli.Context) error {
+	// Parse config files for the extension
+	configPaths := ctx.StringSlice("config")
+	conf, err := config.Parse(configPaths...)
+	if err != nil {
+		return fmt.Errorf("Cannot parse config: %w", err)
+	}
+
+	// Configure the default [slog.Logger]
+	logger, err := slogutils.NewFromConfig(os.Stdout, conf.Logging)
+	if err != nil {
+		return err
+	}
+	slog.SetDefault(logger)
+
+	// Configure database client and set it up for task handlers
+	db, err := newDB(conf)
+	if err != nil {
+		return err
+	}
+	dbclient.SetDB(db)
+	defer db.Close()
+
+	// Create a worker, register handlers and start it up
+	worker := newWorker(conf)
+	worker.HandlersFromRegistry(registry.TaskRegistry)
+	_ = registry.TaskRegistry.Range(func(name string, _ asynq.Handler) error {
+		slog.Info("registered task", "name", name)
+		return nil
+	})
+	slog.Info("worker concurrency", "level", conf.Worker.Concurrency)
+	slog.Info("queue priority", "strict", conf.Worker.StrictPriority)
+	for queue, priority := range conf.Worker.Queues {
+		slog.Info("queue configuration", "name", queue, "priority", priority)
+	}
+
+	return worker.Run()
+}
