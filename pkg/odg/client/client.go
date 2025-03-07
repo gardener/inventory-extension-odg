d16b162ac0562136c48f671a2e4f82cbc7444013
Marin Nikolov <marin.nikolov@sap.com>
2025-03-07 09:41:54 +0200

    odg/client: commit placeholder package for the ODG API client

---
+++ pkg/odg/client/client.go
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package client
+
+// Client is a an API client for the Open Delivery Gear service.
+//
+// TODO: implement me
+type Client struct {
+}
