15e3e278310c628b8d30be7aaf30ee44ad4c28c1
Marin Nikolov <marin.nikolov@sap.com>
2025-03-18 13:06:28 +0200

    odg/tasks: add utility functions for decoding payload and fetching resources

--- pkg/odg/tasks/tasks.go
+++ pkg/odg/tasks/tasks.go
@@ -4,7 +4,14 @@
 
 package tasks
 
-import "errors"
+import (
+	"context"
+	"errors"
+
+	asynqutils "github.com/gardener/inventory/pkg/utils/asynq"
+	"github.com/hibiken/asynq"
+	"github.com/uptrace/bun"
+)
 
 // ErrNoPayload is an error, which is returned by task handlers, which expect a
 // payload, but none was specified.
@@ -19,4 +26,29 @@ var ErrNoQuery = errors.New("no query specified")
 type Payload struct {
 	// Query represents the SQL query to use when fetching orphan resources.
 	Query string `yaml:"query" json:"query"`
+}
+
+// DecodePayload decodes the payload for the given [asynq.Task].
+func DecodePayload(t *asynq.Task) (*Payload, error) {
+	data := t.Payload()
+	if data == nil {
+		return nil, ErrNoPayload
+	}
+
+	var payload Payload
+	if err := asynqutils.Unmarshal(data, &payload); err != nil {
+		return nil, err
+	}
+
+	if payload.Query == "" {
+		return nil, ErrNoQuery
+	}
+
+	return &payload, nil
+}
+
+// FetchResourcesFromDB fetches the resources from the database using the given
+// query into the given dest value.
+func FetchResourcesFromDB(ctx context.Context, db *bun.DB, query string, dest any) error {
+	return db.NewRaw(query).Scan(ctx, dest)
 }
