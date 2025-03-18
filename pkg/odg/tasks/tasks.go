4cce1471047b8312d87f3dacf8d0eea4e401b914
Marin Nikolov <marin.nikolov@sap.com>
2025-03-18 11:58:22 +0200

    odg/tasks: add common errors returned by handlers

--- pkg/odg/tasks/tasks.go
+++ pkg/odg/tasks/tasks.go
@@ -4,6 +4,16 @@
 
 package tasks
 
+import "errors"
+
+// ErrNoPayload is an error, which is returned by task handlers, which expect a
+// payload, but none was specified.
+var ErrNoPayload = errors.New("no payload specified")
+
+// ErrNoQuery is an error, which is returned by task handlers, which expect a
+// query to be provided as part of the payload, but none was specified.
+var ErrNoQuery = errors.New("no query specified")
+
 // Payload represents the payload expected by tasks which report orphan
 // resources to the Open Delivery Gear API.
 type Payload struct {
